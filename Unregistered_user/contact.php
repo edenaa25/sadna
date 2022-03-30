083ec1b15aedf207aa699b53ca2dee753546ce46
edenA <edenaa@mta.ac.il>
2022-03-30 16:11:20 +0300

    no duplicate in database

--- Unregistered_user/contact.php
+++ Unregistered_user/contact.php
@@ -12,20 +12,26 @@ if ($conn->connect_error) {
 $name = $_POST["name"];
 $phone = $_POST["tel"];
 $mail = $_POST["email"];
-if (empty($_POST["message"])){
-    $txt = "no massege";
+
+$dup= mysqli_query($conn, "select * from contact where phone='$phone' ");//בדיקת כפילויות לפי שם
+if(mysqli_num_rows($dup) >0 && $phone!=null){
+    echo "you allready excist in our list, thank you <a  href='../index.html' >press here</a> to return to Home Page";
 }
 else{
-    $txt = $_POST["message"];
+    if (empty($_POST["message"])){
+        $txt = "no massege";
+    }
+    else{
+        $txt = $_POST["message"];
+    }
+
+    $sql= "INSERT IGNORE INTO `contact` (`name`, `phone`, `mail`, `txt`) VALUES ('$name', '$phone', '$maill', '$txt')";
+    $res = $conn->query($sql);
+
+        if ($res == TRUE) {
+            echo "Record updated successfully. <a  href='../index.html' >press here</a> to return to Home Page";
+        } else {
+            echo "Error updating record: " . $conn->error;
+        }
 }
-
-$sql= "INSERT IGNORE INTO `contact` (`name`, `phone`, `mail`, `txt`) VALUES ('$name', '$phone', '$maill', '$txt')";
-$res = $conn->query($sql);
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Record updated successfully. <a  href='../index.html' >press here</a> to return to Home Page";
-      } else {
-        echo "Error updating record: " . $conn->error;
-      }
-
 ?>
